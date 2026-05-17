1dadb2cec275a1079a51474f43c48c09f3646f07
bog666a <reack15819063215@gmail.com>
2026-05-17 12:54:52 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,13 +1,8 @@
-$(call inherit-product, build/target/product/embedded.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Minimal device config for AOSP-based TWRP build
+# BoardConfig.mk contains all recovery-specific settings
 
 PRODUCT_DEVICE := rk3566_r501
 PRODUCT_NAME := omni_rk3566_r501
 PRODUCT_BRAND := rockchip
 PRODUCT_MODEL := OBOOK
 PRODUCT_MANUFACTURER := rockchip
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-TARGET_SCREEN_HEIGHT := 1872
-TARGET_SCREEN_WIDTH := 1404
-DEVICE_RESOLUTION := 1404x1872
